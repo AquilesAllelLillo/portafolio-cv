8cffe26fe2963bada7871af06225a5c07717a602
unknown <aquilesallellillo@gmail.com>
2021-02-03 17:35:42 -0300

    Styles y estructura del Navbar

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Footer = () => (
+  <div id="footer">
+    <div className="inner">
+      <ul className="icons">
+        <li>
+          <a href="#" className="icon fa-twitter">
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon fa-github">
+            <span className="label">Github</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon fa-dribbble">
+            <span className="label">Dribbble</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" className="icon fa-envelope-o">
+            <span className="label">Email</span>
+          </a>
+        </li>
+      </ul>
+      <ul className="copyright">
+        <li>© {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.com">Gatsby</a>
+        </li>
+        <li>&copy; Gatsby Starter Strata</li>
+        <li>
+          Design: <a href="http://html5up.net">HTML5 UP</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Footer
